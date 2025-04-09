b6e7b37c4580c3b8f0126fd75e889c274879c4f6
tetedacier <tetedacier@proton.me>
2025-04-09 05:12:43 +0200

    Add basic yaml reading suport

---
+++ yamlReader.ts
@@ -0,0 +1,41 @@
+import { parse } from "jsr:@std/yaml";
+const textDecoder = new TextDecoder();
+Promise.allSettled([
+  Deno.readFile("./minimal.tokens.yml"),
+  Deno.readFile(".tokens.yml"),
+  Deno.readFile("./composite.tokens.yml"),
+]).then(([minimalTokens, tokens, compositeTokens]) => {
+  const fileErrors: {
+    minimalTokens?: Error;
+    tokens?: Error;
+    compositeTokens?: Error;
+  } = Object.create(null);
+
+  [
+    { minimalTokens },
+    { tokens },
+    { compositeTokens },
+  ].forEach(
+    (
+      fileStream,
+    ) => {
+      const fileReference = Object.keys(fileStream)[0] as
+        | "minimalTokens"
+        | "tokens"
+        | "compositeTokens";
+      const fileResolver = fileStream[fileReference] as PromiseSettledResult<
+        Uint8Array<ArrayBuffer>
+      >;
+      if (fileResolver.status === "rejected") {
+        fileErrors[fileReference] = new Error("cannot read content", {
+          cause: {
+            reason: fileResolver.reason,
+            source: fileReference,
+          },
+        });
+      } else {
+        console.log(parse(textDecoder.decode(fileResolver.value)));
+      }
+    },
+  );
+});
