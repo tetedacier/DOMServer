5830d16c6e7dafee318041f570f084180dc429de
Fabien Jacq <f.jacq@testamento.fr>
2025-04-13 15:31:53 +0200

    Do not forward own message

--- src/socket.ts
+++ src/socket.ts
@@ -46,32 +46,4 @@ Deno.serve({ port: 3000, hostname: "127.0.0.1" }, async (req) => {
       { status: 404 },
     );
   }
-
-  // if (req.headers.get("upgrade") != "websocket") {
-  //   return new Response(null, { status: 501 });
-  // }
-
-  // const { socket, response } = Deno.upgradeWebSocket(req);
-  // socket.addEventListener("open", () => {
-  //   const url = new URL(req.url);
-  //   console.log(
-  //     `client[?] connected on hook ${url.pathname.replace(/^\//, "")}`,
-  //   );
-  // });
-
-  // socket.addEventListener("close", () => {
-  //   const url = new URL(req.url);
-  //   console.log(
-  //     `client[?] disconnected from hook ${url.pathname.replace(/^\//, "")}`,
-  //   );
-  // });
-
-  // socket.addEventListener("message", (event) => {
-  //   console.log(event.data);
-  //   if (event.data === "ping") {
-  //     socket.send("pong");
-  //   }
-  // });
-
-  // return response;
 });
