8e61b23f7b5fec8148261f533b3e88b529deeb82
Fabien Jacq <f.jacq@testamento.fr>
2025-05-01 08:57:43 +0200

    Update basic secrets value inference

--- src/conf.ts
+++ src/conf.ts
@@ -1,1 +1,7 @@
-export const JWT_SECRET = "your-secret-key";
+/**
+  TODO:
+  - A test to ensure non local environment provide all env keys
+  - A procedure to handle SECRET update
+*/
+export const JWT_SECRET = process.env.JWT_SECRET || "your-secret-key";
+export const PASSWORD_SECRET = process.env.PASSWORD_SECRET || "your-password-secret-key";
